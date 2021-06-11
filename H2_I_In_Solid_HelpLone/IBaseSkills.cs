3bb9fd966996fc15fda858cdddad06d619d3a480
Joachim <joac3146@zbc.dk>
2021-06-11 10:49:23 +0200

    Added comment

--- H2_I_In_Solid_HelpLone/IBaseSkills.cs
+++ H2_I_In_Solid_HelpLone/IBaseSkills.cs
@@ -10,6 +10,8 @@ namespace H2_I_In_Solid_HelpLone
     {
         //This interface is responsible for the base skills
         //The reason why i made this an interface, is to be able to add more base skills, without touching the super class
+        //And another reason is, if I want to make an NPC, then that character should not necessarily have a contract with the base skills,
+        // the npc might only need a IQuest contract, or ITalk
         public void Heal()
         {
             Console.WriteLine("I'm healing");
